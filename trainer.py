a5b238c3b084871dd523f09e4556fa0678ec1ffd
Pawel Paradysz <pa.paradysz@gmail.com>
2021-02-10 15:14:43 +0000

    trainer2

--- trainer.py
+++ trainer.py
@@ -9,11 +9,11 @@ def loaders(dataset_path, val_frac=.2, batch_size=8, image_size=512, doses=dose2
     val_len = int(len(dataset) * val_frac)
     lengths = [len(dataset) - val_len, val_len]
     datasets = random_split(dataset, lengths=lengths)
-    return DataLoader(dataset[0], batch_size=batch_size*2), \
-           DataLoader(dataset[1], batch_size=batch_size*2)
+    return DataLoader(datasets[0], batch_size=batch_size*2), \
+           DataLoader(datasets[1], batch_size=batch_size*2)
 
 def reshape_batch(batch):
-    n = loaders[0].batch_size/2
+    n = batch.shape[0]/2
     return batch[:n], batch[n:]
 
 def train(loaders, vgg_enc, epochs=1,
@@ -25,7 +25,7 @@ def train(loaders, vgg_enc, epochs=1,
     for epoch_num in range(epochs):
         model.train()
         for batch in loaders[0]:
-            loss_c, loss_s = model(reshape_batch(batch))
+            loss_c, loss_s = model(*reshape_batch(batch))
             print(loss_c, loss_s)
   
     
