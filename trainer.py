cf44c88c173966b56e1b7157671185a47804c163
Pawel Paradysz <pa.paradysz@gmail.com>
2021-02-10 14:21:09 +0000

    trainer_fix

--- trainer.py
+++ trainer.py
@@ -4,7 +4,7 @@ from net import Transferrer, decoder, vgg_enc
 from util import *
 from tqdm import tqdm
 
-def loaders(dataset_path, val_frac=.2, batch_size=8, doses=dose2locs.keys()):
+def loaders(dataset_path, val_frac=.2, batch_size=8, size=512, doses=dose2locs.keys()):
     dataset = ImageDataset(dataset_path, image_size=image_size, doses=doses)
     val_len = int(len(dataset) * val_frac)
     lengths = [len(dataset) - val_len, val_len]
@@ -16,7 +16,7 @@ def reshape_batch(batch):
     n = loaders[0].batch_size/2
     return batch[:n], batch[n:]
 
-def train(loaders, vgg_path=None, size=512, epochs=1,
+def train(loaders, vgg_path=None, epochs=1,
           decoder=decoder, alpha=1.0):
 
     opt = optim.RAdam(decoder.parameters())
