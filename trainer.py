dfb431333c0dbae5d726ee1b1f9970e87bc071df
Pawel Paradysz <pa.paradysz@gmail.com>
2021-02-12 10:25:33 +0000

    norm

--- trainer.py
+++ trainer.py
@@ -11,8 +11,8 @@ from util import *
 
 
 def loaders(dataset_path, val_frac=.2, batch_size=8, image_size=512, doses=dose2locs.keys(),
-            aug_prob=0.):
-    dataset = ImageDataset(dataset_path, image_size=image_size, doses=doses, aug_prob=aug_prob)
+            aug_prob=0., norm_f=None):
+    dataset = ImageDataset(dataset_path, image_size=image_size, doses=doses, aug_prob=aug_prob, norm_f=norm_f)
     val_len = int(len(dataset) * val_frac)
     lengths = [len(dataset) - val_len, val_len]
     datasets = random_split(dataset, lengths=lengths)
